3df29305caa2789bbecf374e633786d928f11186
Lorrayne Paraiso Carneiro Flor <lorrayneparaisoc@gmail.com>
2018-09-02 18:37:47 -0300

    Create project

---
+++ MemeMeV2/MemeMeV2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeMeV2
+//
+//  Created by Lorrayne Paraiso  on 02/09/18.
+//  Copyright © 2018 Lorrayne Paraiso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
